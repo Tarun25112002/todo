f54868e69835d5aca85a8a5b90b7dfacc4c16ae3
Tarun Kumar Jha <jhatarun14082002@gmail.com>
2025-05-04 01:39:50 +0530

    Styling improvements for the frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
-
+import "./App.css";
+import "./index.css";
 import { CreateTodo } from "./components/CreateTodo";
 import { Todos } from "./components/Todos";
 
